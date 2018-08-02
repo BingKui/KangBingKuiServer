d2641e4265857ba24de60073a17a687530dd88fa
康兵奎 <178974662@qq.com>
2018-08-02 10:38:37 +0800

    add uptoken

--- app/controller/qiniu.js
+++ app/controller/qiniu.js
@@ -2,14 +2,16 @@
 const Controller = require('egg').Controller;
 const qiniu = require('qiniu');
 
-const accessKey = '5jI9Vhnfz7SiVeyzvBH82E5sDqden9aieqZHEzd8-n';
+const accessKey = '5jI9Vhnfz7SiVeyzvBH82E5sDqden9aieqZHEzd8';
 const secretKey = 'F_RJfQCqEqs7TQ721AuzTNT0I6QtU497_03smNqO';
 const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 
 class QiniuController extends Controller {
   async uptoken() {
     const options = {
-        scope: 'kangbingkui-wx'
+        scope: 'kangbingkui-wx',
+        key: 'wx-yd',
+        expires: 7200
     };
     const putPolicy = new qiniu.rs.PutPolicy(options);
     const uptoken = putPolicy.uploadToken(mac);
